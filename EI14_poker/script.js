6bc86b996a519c0b9c719d838a3c15739af15a41
Juliettedqb <juliettedqb@gmail.com>
2023-02-02 18:23:14 +0100

    add step 3 poker game

--- EI14_poker/script.js
+++ EI14_poker/script.js
@@ -3,44 +3,53 @@
 
 let numbers =["1","2","3","4","5","6","7","8","9","10","V","D","R"];
 let symbols = ["♠","♥","♦","♣"];
-let cards = [];
+let allCards = [];
 
 function createDeck() {
   for (let i = 0; i < symbols.length; i++) {
-    numbers.forEach((e) => cards.push(e + symbols[i]));
+    numbers.forEach((e) => allCards.push(e + symbols[i]));
   }
 
-  const shuffledArray = cards.sort((a, b) => 0.5 - Math.random());
+  const shuffledArray = allCards.sort((a, b) => 0.5 - Math.random());
 
-  return cards;
+  return allCards;
 }
-
 let deck = createDeck();
-console.log(deck)
 
 //ETAPE 2
 
 function deal(n) {
   return deck.splice(-n);
 }
+const player1 = deal(2);
+const player2 = deal(2);
 
-const player1 = deal(2)
-const player2 = deal(2)
-
-console.log(player1, player2)
-console.log(deck)
-
-  //add html stuff
+  //add html 
   function playerOneHand() {
     document.getElementById("player1").innerHTML = player1;
-
   }
   function playerTwoHand() {
     document.getElementById("player2").innerHTML = player2;
-
   }
 
 //ETAPE 3
+
+function flop() {
+  deck.pop();
+  const firstRound = deal(3);
+  deck.pop();
+  const secondRound = deal(1);
+  deck.pop();
+  const thirdRound = deal(1);
+  const cards = firstRound + secondRound + thirdRound;
+  return cards
+}
+const cards = flop()
+
+  //add html 
+  function checkoutFlop() {
+    document.getElementById("flop").innerHTML = cards;
+  }
 
 
 
